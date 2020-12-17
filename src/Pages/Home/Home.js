0f3e1f96bbd31668e4cea1ecd7b2d9160fa0c563
Hélio Neto <helioneto29@hotmail.com>
2020-12-17 13:34:52 -0300

    Home atualizada

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -30,6 +30,47 @@ function Home(){
                     </div>
                 </div>
             </div>
+                <div id="esquerda">
+                    <img src="/Imagens/Linha.jpg" alt="Imagem2"></img>
+                    <div id="menu">
+                        <div id="linha">
+                            <h1>Como funciona</h1>
+                        </div>
+                        <div id="linha">
+                            <div id="Imagem1Menu">
+                                <img src="/Imagens/Menu1.jpg" alt="Imagem2"></img>
+                            </div>
+                            <div id="Textos1Menu">
+                                <h1>01</h1>
+                                <h3>Você tem uma necessidade de controle de dados</h3>
+                                <h5>Você entende a importância da gestão por meio da tecnologia e dos dados e precisa de um software que não exija muito esforço de adequação</h5>
+                            </div>
+                        </div>
+                        <div id="linha">
+                            <div id="Textos2Menu">
+                                <h1>02</h1>
+                                <h3>A ropeup desenvolve seu software</h3>
+                                <h5>Criamos o software personalizado para as necessidades da sua empresa</h5>
+                            </div>
+                            <div id="Imagem1Menu">
+                                <img src="/Imagens/Menu2.jpg" alt="Imagem2"></img>
+                            </div>
+                        </div>
+                        <div id="linha">
+                            <div id="Imagem1Menu">
+                                <img src="/Imagens/Menu3.jpg" alt="Imagem2"></img>
+                            </div>
+                            <div id="Textos3Menu">
+                                <h1>03</h1>
+                                <h3>Você administra seus dados de maneira eficiente</h3>
+                                <h5>Pelo computador ou celular, no escritório ou na praia, seus dados atualizados e trabalhos para sua melhor análise e gestão</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="botão">
+                    <Button variant="danger">Quero alavancar os meus resultados</Button>
+                </div>
             <Footer/>
         </div>
     );
