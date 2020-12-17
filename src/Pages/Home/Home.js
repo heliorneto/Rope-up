761c541e6dd7b4f943dd85cdbd7da31aa634d64a
Hélio Neto <helioneto29@hotmail.com>
2020-12-17 13:55:01 -0300

    Como funciona da Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,7 +13,9 @@ function Home(){
                     <div id="Textos1">
                         <h1>Personalize seu software empresarial</h1>
                         <h4>Trabalhe com o que é realmente importante dentro de um software de gestão de maneira integrada e eficiente</h4>
-                        <Button variant="danger">Quero um software personalizado</Button>
+                        <div id="BotãoGeral">
+                            <Button variant="danger">Quero um software personalizado</Button>
+                        </div>
                     </div>
                     <div id="Imagem1">
                         <img src="/Imagens/Imagem1.jpg" alt="Imagem1"></img>
@@ -26,7 +28,9 @@ function Home(){
                     <div id="Textos2">
                         <h1>Controle os dados da sua empresa onde estiver</h1>
                         <h4>Seu software de gestão intuitivo e fácil. Para você analisar os resultados da sua empresa em qualquer lugar</h4>
-                        <Button variant="danger">Conheça mais sobre nossos trabalhos</Button>
+                        <div id="BotãoGeral">
+                            <Button variant="danger">Conheça mais sobre nossos trabalhos</Button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -68,7 +72,7 @@ function Home(){
                         </div>
                     </div>
                 </div>
-                <div id="botão">
+                <div id="botãoAlavancar">
                     <Button variant="danger">Quero alavancar os meus resultados</Button>
                 </div>
             <Footer/>
