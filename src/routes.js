2dc092b02b4d18ccb8a9964f5fefc52df3d9aa66
Hélio Neto <helioneto29@hotmail.com>
2020-12-21 15:08:24 -0300

    Adding route Blog

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
 import Cadastro from './Pages/Cadastro';
+import Blog from './Pages/Blog';
 
 function Routes() {
     return(
@@ -11,6 +12,7 @@ function Routes() {
                 <Route path="/login" component={Login}/>
                 <Route path="/home" component={Home}/>
                 <Route path="/cadastro" component={Cadastro}/>
+                <Route path='/blog' component={Blog}/>
             </Switch>
         </BrowserRouter>
     );
