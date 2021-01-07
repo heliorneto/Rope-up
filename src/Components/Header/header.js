9756dcd8d70c5d551831da907cc7c05e002c3a63
Hélio Neto <helioneto29@hotmail.com>
2021-01-07 10:21:06 -0300

    fixed header removing bootstrap

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { Nav, Navbar, FormControl, InputGroup, Button, Dropdown}from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import Logo from '../../Components/Logo/logo.js';
 import "./header.css";
 
 function Header(){
     return(
-        <div id="navbar" expand="lg">
-            <div id="Logo" href="/home">
+        <nav id="navbar" expand="lg">
+            <ul id="Logo" href="/home">
                 <Logo width="120" height="120" color="#000" fontSize="30"/>
-            </div>
-            <div id="options">
-                <div id="comoFunciona">
-                    <h3>Como Funciona</h3>
-                </div>
-                <div id="sobre">
-                    <h3>Sobre</h3>
-                </div>
-                <div id="blog">
-                    <h3>Blog</h3>
-                </div>
-                <div id="ajuda">
-                    <h3>Ajuda</h3>
-                </div>
-                <div id="login">
-                    <h3><b>Login</b></h3>
-                </div>
-            </div>
-        </div>
+            </ul>
+            <ul id="options">
+                <li id="comoFunciona">
+                    <a href='/home'>Como Funciona</a>
+                </li>
+                <li id="sobre">
+                    <a href='/login'>Sobre</a>
+                </li>
+                <li id="blog">
+                    <a href='/blog'>Blog</a>
+                </li>
+                <li id="ajuda">
+                    <a href='/login'>Ajuda</a>
+                </li>
+                <li id="login">
+                    <a href='/login'><b>Login</b></a>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
