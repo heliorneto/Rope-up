dd6535f985bf0b83bd1d6edb28b588efb7974471
Pedro Dias <pedrodias504@gmail.com>
2021-01-11 13:27:49 -0300

    Removed the article link element and added direct click ability to the card

--- src/Components/BlogCard/blog_card.js
+++ src/Components/BlogCard/blog_card.js
@@ -11,12 +11,9 @@ This component takes the following props:
 
 function BlogCard(props){
     return (
-        <div className="bcard-container">
+        <div className="bcard-container" onClick={()=>window.location = props.link}>
             <img src={props.coverImage} alt={props.coverAlt}  className="bcard-cover"/>
-            <div className="bcard-content">
-                <h3 className="bcard-title">{props.title}</h3>
-                <a href={props.link} className="bcard-link">leia mais...</a>
-            </div>
+            <h3 className="bcard-title">{props.title}</h3>
         </div>
     );
 }
