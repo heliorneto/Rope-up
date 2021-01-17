e0e8ce843d9a38f23c05369eafdbdafa6cd3d444
Hélio Neto <helioneto29@hotmail.com>
2021-01-16 22:54:32 -0300

    capture page

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
+import Capture from './Pages/Capture';
 import Register from './Pages/Register';
 import Blog from './Pages/Blog';
 
@@ -11,6 +12,7 @@ function Routes() {
             <Switch>
                 <Route path="/login" component={Login}/>
                 <Route path="/home" component={Home}/>
+                <Route path="/capture" component={Capture}/>
                 <Route path="/cadastro" component={Register}/>
                 <Route path='/blog' component={Blog}/>
             </Switch>
