edd6b13b661e8775af97a7aa408dae23531d29eb
Pedro Dias <pedrodias504@gmail.com>
2021-01-17 12:44:53 -0300

    Finished implementing carrousel slide animations

--- src/Components/Carrousel/carrousel.js
+++ src/Components/Carrousel/carrousel.js
@@ -5,13 +5,15 @@ import './carrousel.css';
 
 */
 
-class BlogCollection extends React.Component{
+class Carrousel extends React.Component{
     constructor(props){
         super(props);
         this.numPages = Math.ceil(props.children.length/3);
+        this.animationDuration = 750; // Animation duration in milliseconds
         this.state = {
             currentPage: 1,
-            animate: false //Variable to control the carrousel slide animation
+            previousPage: 1,
+            animationDirection: null //Controls the direction of the carrousel animation
         };
         this.nextPage = this.nextPage.bind(this);
         this.previousPage = this.previousPage.bind(this);
@@ -19,19 +21,39 @@ class BlogCollection extends React.Component{
     }
 
     nextPage(){
-        let newPage = this.state.currentPage + 1;
+        const previousPage = this.state.currentPage;
+        let newPage = previousPage + 1;
         if(newPage > this.numPages){
             newPage = 1;
         }
-        this.setState({currentPage: newPage, animate: true});
+        this.setState({
+            currentPage: newPage,
+            previousPage: previousPage,
+            animationDirection: "right"
+        });
+        setTimeout(()=>this.setState({
+            currentPage: this.state.currentPage,
+            previousPage: this.state.currentPage, 
+            animationDirection: null
+        }), this.animationDuration - 20);
     }
 
     previousPage(){
-        let newPage = this.state.currentPage - 1;
+        const previrousPage = this.state.currentPage;
+        let newPage = previrousPage - 1;
         if(newPage <= 0){
             newPage = this.numPages;
         }
-        this.setState({currentPage: newPage, animate: true});
+        this.setState({
+            currentPage: newPage,
+            previousPage: previrousPage,
+            animationDirection: "left"
+        });
+        setTimeout(()=>this.setState({
+            currentPage: this.state.currentPage,
+            previousPage: this.state.currentPage,
+            animationDirection: null
+        }), this.animationDuration - 20);
     }
 
     genPageIndex(){
@@ -41,19 +63,25 @@ class BlogCollection extends React.Component{
 
     render(){
         const begin = (this.state.currentPage - 1) * 3;
-        const end = begin + 3;
-        const slicedItems = this.props.children.slice(begin,end);
+        const slicedItems = this.props.children.slice(begin,begin + 3);
+        const pbegin = (this.state.previousPage - 1) * 3;
+        const previrousItems = this.props.children.slice(pbegin,pbegin + 3);
         return (
             <div className="carrousel-container">
                 <button className="carrousel-next" onClick={()=>this.previousPage()}/>
                 <div className="carrousel-items">
                     {slicedItems}
                 </div>
-                {(this.state.animate)? <div className="carrousel-animator-duplicate">{slicedItems}</div>: null}
+                {
+                    (this.state.animationDirection) && (
+                    <div className={`carrousel-animator-duplicate carrousel-slide-${this.state.animationDirection}`}>
+                        {previrousItems}
+                    </div>)
+                }
                 <button className="carrousel-previous" onClick={()=>this.nextPage()}/>
             </div>
         );
     }
 }
 
-export default BlogCollection;
+export default Carrousel;
