4b1c5a813ae0148ad4c59ad3578d636a43c0d2d4
Pedro Dias <pedrodias504@gmail.com>
2021-01-18 12:05:50 -0300

    Added hover and selected animations to the Header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,7 +3,13 @@ import { useHistory } from 'react-router-dom';
 import Logo from '../../Components/Logo/logo.js';
 import "./header.css";
 
-function Header(){
+/*
+This component takes a prop named "selectedPage" that should be set to the 
+name of the current page (on lowercase). By setting it, the current page
+link will receive a different colour in the Header.
+*/
+
+function Header(props){
     let history = useHistory();
     return(
         <nav id="navbar" expand="lg">
@@ -15,13 +21,13 @@ function Header(){
                     <a href='/home'>Como Funciona</a>
                 </li>
                 <li id="blog">
-                    <a href='/blog'>Blog</a>
+                    <a href='/blog' style={(props.selectedPage === "blog")? {color: "#023B59"}: {}}>Blog</a>
                 </li>
                 <li id="sobre">
-                    <a href='/login'>Sobre</a>
+                    <a href='/sobre' style={(props.selectedPage === "sobre")? {color: "#023B59"}: {}}>Sobre</a>
                 </li>
                 <li id="login">
-                    <a href='/login'><b>Login</b></a>
+                    <a href='/login' style={(props.selectedPage === "login")? {color: "#023B59"}: {}}><b>Login</b></a>
                 </li>
             </ul>
         </nav>
