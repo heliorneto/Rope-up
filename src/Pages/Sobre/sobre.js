3cdd098f9c3e4a3a2ffa17dfb31d94070d15ac1d
Iago <iaggo.gui@gmail.com>
2021-01-18 11:56:35 -0300

    Sobre page correction

--- src/Pages/Sobre/sobre.js
+++ src/Pages/Sobre/sobre.js
@@ -10,10 +10,10 @@ function Sobre() {
             <div id="sobre-general">
                 <div id="introducao-text">
                     <div id="introducao-title">
-                        <h1>Sobre a Roupeup</h1>
+                        <h1>Sobre a Ropeup</h1>
                     </div>
                     <div id="introducao">
-                        <h3 id="text">Acreditamos na utilização da <b>tecnologia para uma gestão mais inteligente.</b>Nossos especialistas desenvolvedores são conectados com grandes universidaes do país sempre levando <b>para seus projetos.</b></h3>
+                        <h3 id="text">Acreditamos na utilização da <b>tecnologia para uma gestão mais inteligente. </b>Nossos especialistas desenvolvedores são conectados com grandes universidaes do país sempre levando <b>para seus projetos.</b></h3>
                         <h3 id="text">Nós movemos pelo <b>sucesso do cliente,</b> sempre buscando o resultado e a melhor experiência detro e fora do projeto.</h3>
                     </div>
                 </div>
