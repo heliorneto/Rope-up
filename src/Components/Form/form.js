d6aa64b1ddeca1a41632d4280a231a2c4e7623cb
Pedro Dias <pedrodias504@gmail.com>
2021-02-09 09:48:08 -0300

    Improved accessibility by configuring the label's for attribute and input ids

--- src/Components/Form/form.js
+++ src/Components/Form/form.js
@@ -4,22 +4,22 @@ import './form.css';
 function Form(props){
     return (
         <form name={props.name} method='POST' action={props.action} className="form-container">
-            <label>Nome <span className="form-required">*</span></label>
-            <input type="text" name="Name" maxLength="70" required/>
-            <label>Sobrenome <span className="form-required">*</span></label>
-            <input type="text" name="Surname" maxLength="70" required/>
-            <label>Email <span className="form-required">*</span></label>
-            <input type="email" name="Email" required/>
-            <label>Telefone <span className="form-required">*</span></label>
-            <input type="tel" name="Phone" required/>
-            <label>Telefone (alternativo)</label>
-            <input type="tel" name="Phone2"/>
-            <label>Cargo <span className="form-required">*</span></label>
-            <input type="text" name="Role" maxLength="40" required/>
-            <label>Nome da empresa <span className="form-required">*</span></label>
-            <input type="text" name="Enterprise" maxLength="70" required/>
-            <label>Número de funcionários <span className="form-required">*</span></label>
-            <input type="number" name="NumEmployees" min="1" max="65535" required/>
+            <label htmlFor="nome">Nome <span className="form-required">*</span></label>
+            <input id="nome" type="text" name="Name" maxLength="70" required/>
+            <label htmlFor="sobrenome">Sobrenome <span className="form-required">*</span></label>
+            <input id="sobrenome" type="text" name="Surname" maxLength="70" required/>
+            <label htmlFor="email">Email <span className="form-required">*</span></label>
+            <input id="email" type="email" name="Email" required/>
+            <label htmlFor="telefone">Telefone <span className="form-required">*</span></label>
+            <input id="telefone" type="tel" name="Phone" required/>
+            <label htmlFor="telefone-alternativo">Telefone (alternativo)</label>
+            <input id="telefone-alternativo" type="tel" name="Phone2"/>
+            <label htmlFor="cargo">Cargo <span className="form-required">*</span></label>
+            <input id="cargo" type="text" name="Role" maxLength="40" required/>
+            <label htmlFor="empresa">Nome da empresa <span className="form-required">*</span></label>
+            <input id="empresa" type="text" name="Enterprise" maxLength="70" required/>
+            <label htmlFor="num-funcionarios">Número de funcionários <span className="form-required">*</span></label>
+            <input id="num-funcionarios" type="number" name="NumEmployees" min="1" max="65535" required/>
             <input type="submit" value="Fale com um especialista"/>
         </form>
     );
