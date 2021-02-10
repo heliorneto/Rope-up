f6c947828f355529c3b43789579201b59adb764b
Pedro Dias <pedrodias504@gmail.com>
2021-02-09 23:26:27 -0300

    Removed a unnecessary import on the 'Sobre' page

--- src/Pages/Sobre/sobre.js
+++ src/Pages/Sobre/sobre.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../Components/Header/header';
 import Footer from '../../Components/Footer/footer';
 import "./sobre.css";
-import TextBar from '../../Components/TextBar/textBar';
 
 function Sobre() {
     return(
