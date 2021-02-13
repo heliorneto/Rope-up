93bbc8c0db55c0b5d896b04ef0d51b46d56e0126
Pedro Dias <pedrodias504@gmail.com>
2021-02-13 16:13:43 -0300

    Added scroll indicator to the article page

--- src/Pages/Artigos/artigos.js
+++ src/Pages/Artigos/artigos.js
@@ -8,15 +8,39 @@ class Artigos extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            showProgress: false,
+            progress: 0
         };
+        this.updateProgress = this.updateProgress.bind(this);
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll',this.updateProgress);
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('scroll',this.updateProgress);
+    }
+
+    updateProgress(){
+        const scrollOffset = (document.body.scrollTop)? document.body.scrollTop: document.documentElement.scrollTop;
+        const maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const progress = (scrollOffset/maxScroll)*100;
+        if(scrollOffset > 50){
+            this.setState({showProgress: true, progress: progress});
+        }else{
+            this.setState({showProgress: false, progress: 0});
+        }
     }
 
     render(){
         return (
             <div id="page-artigos">
-                <Header/>
-                    <Article baseServerUrl="http://localhost:1337/artigos" baseMediaUrl="http://localhost:1337" articleID={this.props.match.params.id}/>
+                <Header showProgress={true}/>
+                <div className={(this.state.showProgress)? "read-progress-bar": "hide-progress"}>
+                    <div id="read-progress" style={{width: this.state.progress + "%"}}/>
+                </div>
+                <Article baseServerUrl="http://localhost:1337/artigos" baseMediaUrl="http://localhost:1337" articleID={this.props.match.params.id}/>
                 <Footer/>
             </div>
         );
