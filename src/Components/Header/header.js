3bb5f461815f0aafcea055716217f9e98df20642
Pedro Dias <pedrodias504@gmail.com>
2021-03-02 12:37:14 -0300

    Added basic condition render to Header component targeting mobile users
    Initial work on a responsive Header component. The component now loads on a different style in mobile phones

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -10,46 +10,60 @@ link will receive a different colour in the Header.
 */
 
 function Header(props){
+    const isPhone = window.matchMedia("(max-width: 600px)").matches;
+    const scrollOffset = (isPhone)? 10: 50;
     let history = useHistory();
     const [shrink, setShrink] = useState(false);
 
-    const shrinkHeader = ()=>{
-        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-            setShrink(true);
-        }else{
-            setShrink(false);
-        }
-    }
-
     useEffect(()=>{
+    
+        const shrinkHeader = ()=>{
+            if(document.body.scrollTop > scrollOffset || document.documentElement.scrollTop > scrollOffset){
+                setShrink(true);
+            }else{
+                setShrink(false);
+            }
+        }
+
         window.addEventListener('scroll',shrinkHeader);
         return ()=>{window.removeEventListener('scroll',shrinkHeader);};
-    },[]);
+    },[scrollOffset]);
 
-    return(
-        <nav id="header" className={shrink? 'small': ''}>
-            <div id="logo" onClick={()=>{history.push("/home")}}>
-                {
-                    shrink? <Logo width="80" height="80" color="#000" fontSize="28"/>:
-                    <Logo width="120" height="120" color="#000" fontSize="30"/>
-                }
-            </div>
-            <ul id="options">
-                <li id="comoFunciona">
-                    <a href='/home#link'>Como Funciona</a>
-                </li>
-                <li id="blog">
-                    <a href='/blog' style={(props.selectedPage === "blog")? {color: "#023B59"}: {}}>Blog</a>
-                </li>
-                <li id="sobre">
-                    <a href='/sobre' style={(props.selectedPage === "sobre")? {color: "#023B59"}: {}}>Sobre</a>
-                </li>
-                <li id="login">
-                    <a href='/login' style={(props.selectedPage === "login")? {color: "#023B59"}: {}}><b>Login</b></a>
-                </li>
-            </ul>
-        </nav>
-    );
+    if(!isPhone){
+        return(
+            <nav id="header" className={shrink? 'small': ''}>
+                <div id="logo" onClick={()=>{history.push("/home")}}>
+                    {
+                        (shrink)? <Logo width="80" height="80" color="#000" fontSize="28"/>:
+                        <Logo width="120" height="120" color="#000" fontSize="30"/>
+                    }
+                </div>
+                <ul id="options">
+                    <li id="comoFunciona">
+                        <a href='/home#link'>Como Funciona</a>
+                    </li>
+                    <li id="blog">
+                        <a href='/blog' style={(props.selectedPage === "blog")? {color: "#023B59"}: {}}>Blog</a>
+                    </li>
+                    <li id="sobre">
+                        <a href='/sobre' style={(props.selectedPage === "sobre")? {color: "#023B59"}: {}}>Sobre</a>
+                    </li>
+                    <li id="login">
+                        <a href='/login' style={(props.selectedPage === "login")? {color: "#023B59"}: {}}><b>Login</b></a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }else{
+        return (
+            <nav id="header" className={shrink? 'small': ''}>
+                <div id="logo" onClick={()=>{history.push("/home")}}>
+                    <Logo width="80" height="80" color="#000" fontSize="28"/>
+                </div>
+                <button id="menu-icon"/>
+            </nav>
+        );
+    }
 }
 
 export default Header;
