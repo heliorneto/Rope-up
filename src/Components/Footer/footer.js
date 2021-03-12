9b2c289feca2184ee00b190f5b33638ef7ce0f03
Iago <iaggo.gui@gmail.com>
2021-03-11 23:43:19 -0300

    Phone footer and sobre

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -3,21 +3,30 @@ import Logo from './../../Components/Logo/logo.js';
 import "./footer.css";
 
 function Footer(){
-    return(
-        <nav id="nav-footer">
-            <ul id="logo-footer" href="/home">
-                <Logo id="logo" color='#ffffff' width="130" heigth="120" fontSize="40"/>
-            </ul>
-            <ul id="opcoes-footer">
-                <li id="onde-estamos-footer">
-                    <a href=''>Onde Estamos</a>
-                </li>
-            </ul>
-            <ul id="direitos">
-                <p><small>©2020 Todos os direitos reservados</small></p>
-            </ul>
-        </nav>
-    );
+
+    const isPhone = window.matchMedia("(max-width: 600px)").matches;
+
+    if(!isPhone){
+        return(
+            <nav id="nav-footer">
+                <div id="upper-footer">
+                    <Logo id="logo" color='#ffffff' width="130" heigth="120" fontSize="40"/>
+                    <a id="onde-estamos-footer" href=''>Onde Estamos</a>
+                </div>
+                <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
+            </nav>
+        );
+    }else{
+        return(
+            <nav id="nav-footer">
+                <div id="upper-footer">
+                    <Logo id="logo" color='#ffffff' width="55" heigth="60" fontSize="23"/>
+                    <a id="onde-estamos-footer" href=''>Onde Estamos</a>
+                </div>
+                <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
+            </nav>
+        );
+    }
 }
 
 export default Footer;
