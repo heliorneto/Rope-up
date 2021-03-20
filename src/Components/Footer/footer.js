bc9c2a0525addc215bbbe4a3c8d2068d9bd7a270
Pedro Dias <pedrodias504@gmail.com>
2021-03-20 14:35:56 -0300

    Added footer link action and fixed shrink refresh bug

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import Logo from './../../Components/Logo/logo.js';
 import "./footer.css";
 
 function Footer(){
 
-    const isPhone = window.matchMedia("(max-width: 600px)").matches;
+    const [isPhone, setPhone] = useState(window.matchMedia("(max-width: 600px)").matches);
+
+    useEffect(()=>{
+        const checkDisplay = () => {
+            setPhone(window.matchMedia("(max-width: 600px)").matches);
+        }
+        window.addEventListener('resize',checkDisplay);
+        return ()=>{
+            window.removeEventListener('resize',checkDisplay);
+        };
+    });
 
     if(!isPhone){
         return(
             <nav id="nav-footer">
                 <div id="upper-footer">
                     <Logo id="logo" color='#ffffff' width="130" heigth="120" fontSize="40"/>
-                    <a id="onde-estamos-footer" href=''>Onde Estamos</a>
+                    <a id="onde-estamos-footer" href='/sobre'>Onde Estamos</a>
                 </div>
                 <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
             </nav>
@@ -21,7 +31,7 @@ function Footer(){
             <nav id="nav-footer">
                 <div id="upper-footer">
                     <Logo id="logo" color='#ffffff' width="55" heigth="60" fontSize="23"/>
-                    <a id="onde-estamos-footer" href=''>Onde Estamos</a>
+                    <a id="onde-estamos-footer" href='/sobre'>Onde Estamos</a>
                 </div>
                 <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
             </nav>
