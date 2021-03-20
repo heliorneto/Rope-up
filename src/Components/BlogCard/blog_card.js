6c2c8a35965fae6592f03ce1cafb6d9b51617b84
Pedro Dias <pedrodias504@gmail.com>
2021-03-20 18:37:22 -0300

    Made the blog card responsive

--- src/Components/BlogCard/blog_card.js
+++ src/Components/BlogCard/blog_card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import './blog_card.css';
 
 /*
@@ -10,8 +11,10 @@ This component takes the following props:
 */
 
 function BlogCard(props){
+    const history = useHistory();
+
     return (
-        <div className="bcard-container" onClick={()=>window.location = props.link}>
+        <div className="bcard-container" onClick={()=>{history.push(props.link)}}>
             <img src={props.coverImage} alt={props.coverAlt}  className="bcard-cover"/>
             <h3 className="bcard-title">{props.title}</h3>
         </div>
