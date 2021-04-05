70cd53021881519444ccb8c76e516749e9cda211
Pedro Dias <pedrodias504@gmail.com>
2021-04-05 17:33:21 -0300

    Fixed duplicated IDs on "Sobre" page

--- src/Pages/Sobre/sobre.js
+++ src/Pages/Sobre/sobre.js
@@ -22,8 +22,8 @@ function Sobre() {
             <div id="sobre-general" >
                 <div id="introducao-text">
                     <h1 id="introducao-title">Sobre a ropeup</h1>
-                    <p id="text">Acreditamos na utilização da <strong>tecnologia para uma gestão mais inteligente.</strong> Nossos especialistas e desenvolvedores são conectados com grandes universidades do país sempre levando <strong>inovação para seus projetos.</strong></p>
-                    <p id="text">Nos movemos pelo <strong>sucesso do cliente,</strong> sempre buscando o resultado e a melhor experiência dentro e fora do projeto.</p>
+                    <p className="text">Acreditamos na utilização da <strong>tecnologia para uma gestão mais inteligente.</strong> Nossos especialistas e desenvolvedores são conectados com grandes universidades do país sempre levando <strong>inovação para seus projetos.</strong></p>
+                    <p className="text">Nos movemos pelo <strong>sucesso do cliente,</strong> sempre buscando o resultado e a melhor experiência dentro e fora do projeto.</p>
                 </div>
                 <img id="image-top" src="/Imagens/Sobre1.png" alt="Sobre1"/>
             </div>
