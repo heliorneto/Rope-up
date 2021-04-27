faa3a14d18889eb086c7fe70b69c835e1f97d113
Pedro Dias <pedrodias504@gmail.com>
2021-04-27 19:39:27 -0300

    Fixed search bar enter function event
    Added a prop to take the function to execute and fixed a bug

--- src/Components/SearchBar/search-bar.js
+++ src/Components/SearchBar/search-bar.js
@@ -5,23 +5,22 @@ import './search-bar.css'
 This component takes the following props:
 - placeholder: The text present by default in the text box
 - width: The width of the text box
+- enterAction: A function to execute after pressing the enter key (receives the input value as parameter)
 */
 
 
 
 function SearchBar(props){
 
-    function searchKey(){
-        document.getElementById("search").addEventListener("keydown", function(event) {
-            if (event.key === "Enter") {
-                alert("You pressed a key inside the input field");
-                
-            }
-          });
+    function searchKey(event){
+        const input = document.getElementById('search-bar').value;
+        if(event.key === "Enter"){
+            props.enterAction(input);
+        }
     }
 
     return(
-            <input type="search" id="search" placeholder={props.placeholder} style={{width:props.width}} onKeyDown= {()=>searchKey()}/>
+            <input type="search" id="search-bar" placeholder={props.placeholder} style={{width:props.width}} onKeyDown= {searchKey}/>
     );
 }
 
