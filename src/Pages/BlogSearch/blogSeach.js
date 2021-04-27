d952ee492894359ac8be715b45c5ab1e6582887b
Pedro Dias <pedrodias504@gmail.com>
2021-04-27 16:36:12 -0300

    Added a search/advanced search page

---
+++ src/Pages/BlogSearch/blogSeach.js
@@ -0,0 +1,155 @@
+import React from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
+import Header from "./../../Components/Header/header";
+import Footer from "./../../Components/Footer/footer";
+import Button from "./../../Components/Button/button";
+import BlogCard from "./../../Components/BlogCard/blog_card";
+import Gallery from "./../../Components/Gallery/gallery";
+import "./blogSearch.css";
+
+export default function BlogSearch(props){
+    const articleRequestURL = "http://localhost:8055/items/article";
+    const mediaRequestURL = "http://localhost:8055/assets/";
+    const searchQuery = parseURL(props.location.search);
+    const [selectedCategories, setSelectedCategories] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [numResults, setNumResults] = useState(0);
+
+    function parseURL(searchString){
+        let vars = searchString.split('&');
+        vars[0] = vars[0].substring(1);
+        let res = {};
+        for(const variable of vars){
+            const nameVal = variable.split('=');
+            res[decodeURIComponent(nameVal[0])] = decodeURIComponent(nameVal[1]);
+        }
+        return res;
+    }
+
+    function parseCategories(){
+        const checkboxes = document.querySelectorAll(".category-check input");
+        let selected = [];
+        for(const checkbox of checkboxes){
+            if(checkbox.checked){
+                selected.push(checkbox.dataset.id);
+            }
+        }
+        setSelectedCategories(selected.join(','));
+    }
+
+    async function getCategories(){
+        const response = await axios.get("http://localhost:8055/items/category/");
+        return response.data.data.map((item)=>(
+            <div key={item.id} className="category-check">
+                <input type="checkbox" data-id={item.id}/>
+                <label>{item.name}</label>
+            </div>
+        ));
+    }
+
+    async function getNumResults(){
+        const query = parseURL(props.location.search);
+        const params = (query.category)? {
+            search: query.q,
+            "filter[category][id][_in]": query.category,
+            meta: "filter_count",
+            fields: "id"
+        }: {
+            search: query.q,
+            meta: "filter_count",
+            fields: "id"
+        };
+        const response = await axios.get(articleRequestURL, {
+            params: params
+        });
+        setNumResults(response.data.meta.filter_count);
+        return response.data.meta.filter_count;
+    }
+
+    async function getResultPage({currentPage, numPageItems}){
+        const query = parseURL(props.location.search);
+        const params = (query.category)? {
+            search: query.q,
+            "filter[category][id][_in]": query.category,
+            meta: "filter_count",
+            fields: "id,title,cover.id,cover.description",
+            limit: numPageItems,
+            page: currentPage
+        }: {
+            search: query.q,
+            meta: "filter_count",
+            fields: "id,title,cover.id,cover.description",
+            limit: numPageItems,
+            page: currentPage
+        };
+        const response = await axios.get(articleRequestURL, {
+            params: params
+        });
+        return response.data.data.map((item)=>{
+            return <BlogCard
+            key={item.id}
+            title={item.title}
+            link={"/blog/artigos/" + item.id}
+            coverImage={mediaRequestURL + item.cover.id}
+            coverAlt={item.cover.description}
+            />
+        });
+    }
+
+    useEffect(()=>{
+        async function loadCategories(){
+            setCategories(await getCategories());
+        }
+        
+        loadCategories();
+    },[]);
+
+    return (
+        <div id="page-blog-search">
+            <Header/>
+                <div id="search-content">
+                    <div id="search-header">
+                        <h2>Resultados da busca por:</h2>
+                        <h1>{searchQuery.q}</h1>
+                        <p>{numResults} posts</p>
+                    </div>
+                    <div id="search-results">
+                        <Gallery
+                        rows={3}
+                        columns={3}
+                        cardSpacing="40px"
+                        countItems={getNumResults}
+                        getPageItems={getResultPage}
+                        />
+                    </div>
+                    <form id="search-advanced" name="search-query" method="get" onChange={parseCategories}>
+                        <h2>Não encontrou o que procura?</h2>
+                        <h3>Refine sua busca:</h3>
+                        <div id="search-controls">
+                            <div id="search-term"className="search-inputs">
+                                <label>Termo de pesquisa: </label>
+                                <input name="q" type="search" placeholder="Entre uma palavra..."/> 
+                            </div>
+                            <div className="search-inputs">
+                                <label for="category">Selecione as categorias de artigos em que deseja procurar: </label>
+                                <input name={(selectedCategories === "")? "": "category"} value={selectedCategories.split(",")} type="hidden"/>
+                                <div id="search-categories">
+                                    {categories}
+                                </div>
+                            </div>
+                            <Button 
+                            clickAction={()=>{}} 
+                            backgroundColor="red" 
+                            text="Pesquisa avançada" 
+                            textColor="white"
+                            width="200px"
+                            height="50px"
+                            />
+                        </div>
+                    </form>
+                </div>
+            <Footer/>
+        </div>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Home from './Pages/Home';
 import Capture from './Pages/Capture';
 import Blog from './Pages/Blog';
+import BlogSearch from './Pages/BlogSearch/blogSeach';
 import Sobre from './Pages/Sobre';
 import Artigos from './Pages/Artigos/artigos';
 
@@ -14,6 +15,7 @@ function Routes() {
                 <Route path="/home" component={Home}/>
                 <Route path="/contato" component={Capture}/>
                 <Route exact path='/blog' component={Blog}/>
+                <Route exact path='/blog/search' component={BlogSearch}/>
                 <Route path="/sobre" component={Sobre}/>
                 <Route path="/blog/artigos/:id" component={Artigos}/>
             </Switch>
