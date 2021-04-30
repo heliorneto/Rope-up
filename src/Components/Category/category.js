f770ea72c920ddc3469431da309db1b68f1854a4
Pedro Dias <pedrodias504@gmail.com>
2021-04-30 14:51:42 -0300

    More fixes to the "Category" component

--- src/Components/Category/category.js
+++ src/Components/Category/category.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
 import './category.css';
 
 /*
 This component takes the following props:
-- title: The title of the category
-- link: The direct link to the category's page
-- topImage: A link to the category's top image
-- coverAlt: A string with alternative text describing the category's cover (accessibility)
+- categoryID: The directus ID of the category
+- name: The name of the category
+- icon: A link to the category's icon
+- iconAlt: A string with alternative text describing the category's icon (accessibility)
 - description: The description of the category
-- leiaMais: A space in case the category contains a "leia mais..."
-
 */
 
 function Category(props){
-    const history = useHistory();
-
     return (
-        <div className="category-container" onClick={()=>{history.push(props.link)}}>
-            <img src={props.topImage} alt={props.coverAlt}  className="category-image"/>
-            <h3 className="category-title">{props.title}</h3>
+        <div className="category-container">
+            <img src={props.icon} alt={props.iconAlt}  className="category-image"/>
+            <h3 className="category-title">{props.name}</h3>
             <h5 className="category-description">{props.description}</h5>
-            <h5 className="category-leia-mais">{props.leiaMais}</h5>
+            <a href={`/blog/search/?q=&category=${props.categoryID}`} className="category-leia-mais">leia mais...</a>
         </div>
     );
 }
