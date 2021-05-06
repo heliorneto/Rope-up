f69bf2457b22e4dfbf43c3b610b0d9ffc6e5ee3d
Pedro Dias <pedrodias504@gmail.com>
2021-05-06 16:20:33 -0300

    Optimized Footer component code

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -6,27 +6,18 @@ import "./footer.css";
 function Footer(){
     const {isPhone} = useMedia();
 
-    if(!isPhone){
-        return(
-            <nav id="nav-footer">
-                <div id="upper-footer">
+    return(
+        <nav id="nav-footer">
+            <div id="upper-footer">
+                {
+                    (isPhone)? <Logo color='#ffffff' width="60" heigth="60" fontSize="26"/>:
                     <Logo color='#ffffff' width="130" heigth="120" fontSize="40"/>
-                    <a id="onde-estamos-footer" href='/sobre'>Onde Estamos</a>
-                </div>
-                <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
-            </nav>
-        );
-    }else{
-        return(
-            <nav id="nav-footer">
-                <div id="upper-footer">
-                    <Logo color='#ffffff' width="60" heigth="60" fontSize="26"/>
-                    <a id="onde-estamos-footer" href='/sobre'>Onde Estamos</a>
-                </div>
-                <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
-            </nav>
-        );
-    }
+                }
+                <a id="onde-estamos-footer" href='/sobre'>Onde Estamos</a>
+            </div>
+            <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
+        </nav>
+    );
 }
 
 export default Footer;
