6bbd6e34b39e48f57b3e7faae622efaeaea4e182
Pedro Dias <pedrodias504@gmail.com>
2021-05-07 18:10:20 -0300

    Renamed the Footer link to "Sobre a Ropeup"

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -13,7 +13,7 @@ function Footer(){
                     (isPhone)? <Logo color='#ffffff' width="60" heigth="60" fontSize="26"/>:
                     <Logo color='#ffffff' width="100" heigth="100" fontSize="36"/>
                 }
-                <a id="onde-estamos-footer" href='/sobre'>Onde Estamos</a>
+                <a id="onde-estamos-footer" href='/sobre'>Sobre a Ropeup</a>
             </div>
             <p id="direitos"><small>©2020 Todos os direitos reservados</small></p>
         </nav>
