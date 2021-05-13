3817f0b13ee4740ac941b9a76490866c8b8697a1
Pedro Dias <pedrodias504@gmail.com>
2021-05-13 00:17:49 -0300

    Configured the CMS to point to the heroku test server

--- src/cms_config.js
+++ src/cms_config.js
@@ -1,6 +1,6 @@
 export default Object.freeze({
-    BASE_URL: "http://localhost",
-    PORT: 8055,
+    BASE_URL: "https://ropeup-cms-test.herokuapp.com",
+    PORT: null,
     getFullURL: function(){
         if(this.PORT){
             return this.BASE_URL + ":" + this.PORT;
