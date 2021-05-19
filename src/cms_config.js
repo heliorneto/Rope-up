156d2cb8a8d8e87fc4932d04e51321f690ded300
Pedro Dias <pedrodias504@gmail.com>
2021-05-19 11:05:46 -0300

    Reconfigured CMS for the test environment

--- src/cms_config.js
+++ src/cms_config.js
@@ -1,6 +1,6 @@
 export default Object.freeze({
-    BASE_URL: "https://www.ropeup.com.br/cms",
-    PORT: null,
+    BASE_URL: "http://localhost",
+    PORT: 8085,
     getFullURL: function(){
         if(this.PORT){
             return this.BASE_URL + ":" + this.PORT;
